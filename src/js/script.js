a9ecdfc56c0098496beeea06815a3ca3345c77a1
AzsaNrD <azsa.nwhy@gmail.com>
2021-08-31 09:20:38 +0700

    sosio

--- src/js/script.js
+++ src/js/script.js
@@ -159,6 +159,44 @@ var x = setInterval(function () {
 
 }, 1000);
 
+// sosio
+var countDateSosio = new Date('August 31 2021 10:30:00').getTime();
+var x = setInterval(function () {
+    var now = new Date().getTime();
+    gap = countDateSosio - now
+
+    var detik = 1000;
+    var menit = detik * 60;
+    var jam = menit * 60;
+    var hari = jam * 24;
+
+    var h = Math.floor(gap / (hari));
+    var j = Math.floor((gap % (hari)) / (jam));
+    var m = Math.floor((gap % (jam)) / (menit));
+    var d = Math.floor((gap % (menit) / (detik)));
+
+    if (h < 10) {
+        h = "0" + h;
+    }
+    if (j < 10) {
+        j = "0" + j;
+    }
+    if (m < 10) {
+        m = "0" + m;
+    }
+    if (d < 10) {
+        d = "0" + d;
+    }
+
+    document.getElementById("sosiodl").innerHTML = h + " Hari, " + j + " Jam, " + m + " Menit, " + d + " Detik.";
+
+    if (gap < 0) {
+        clearInterval(x);
+        document.getElementById("sosiodl").innerHTML = ":v";
+    }
+
+}, 1000);
+
 
 // Jadwal Pelajaran Filter
 const filterItem = document.querySelector('.jadwalFilter');
