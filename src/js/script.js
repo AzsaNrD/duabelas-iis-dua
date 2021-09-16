c6a9e0d6e5aebc3711a4d3cb3f2c198ad3b71743
AzsaNrD <azsa.nwhy@gmail.com>
2021-09-16 08:58:19 +0700

    tugas geo

--- src/js/script.js
+++ src/js/script.js
@@ -151,6 +151,44 @@ var x = setInterval(function () {
 
 }, 1000);
 
+// geografi
+var countDateGeo = new Date('September 26 2021 24:00:00').getTime();
+var x = setInterval(function () {
+    var now = new Date().getTime();
+    gap = countDateGeo - now
+
+    var detik = 1000;
+    var menit = detik * 60;
+    var jam = menit * 60;
+    var hari = jam * 24;
+
+    var h = Math.floor(gap / (hari));
+    var j = Math.floor((gap % (hari)) / (jam));
+    var m = Math.floor((gap % (jam)) / (menit));
+    var d = Math.floor((gap % (menit) / (detik)));
+
+    if (h < 10) {
+        h = "0" + h;
+    }
+    if (j < 10) {
+        j = "0" + j;
+    }
+    if (m < 10) {
+        m = "0" + m;
+    }
+    if (d < 10) {
+        d = "0" + d;
+    }
+
+    document.getElementById("geografi").innerHTML = h + " Hari, " + j + " Jam, " + m + " Menit, " + d + " Detik.";
+
+    if (gap < 0) {
+        clearInterval(x);
+        document.getElementById("geografi").innerHTML = "Telah lewat tenggat waktu!";
+    }
+
+}, 1000);
+
 
 // Jadwal Pelajaran Filter
 const filterItem = document.querySelector('.jadwalFilter');
